93f0ba771b2fd7bb27904e27a5812a76656ef3ea
Utaho Akashi <akashi.uta.1212@gmail.com>
2025-09-10 10:40:38 +0900

    feat: ルーティングの設定

---
+++ frontend/nextjs-project/src/app/discoveries/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+export default function Discovery() {
+  return (
+    <Box>
+      discovery ページ
+    </Box>
+  );
+}
--- frontend/nextjs-project/src/app/page.tsx
+++ frontend/nextjs-project/src/app/page.tsx
@@ -1,10 +1,27 @@
 import { Box } from "@mui/material";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <Box>
-      Hello, Next.js!
-      こんにちは
+      <nav>
+        <ul>
+          <li>
+            <Link href="/list">一覧ページへ</Link>
+          </li>
+          <li>
+            <Link href="/login">ログインページへ</Link>
+          </li>
+          <li>
+            <Link href="/signup">新規登録ページへ</Link>
+          </li>
+          <li>
+            <Link href="/discoveries/hokkaido-university-poplar-avenue">
+              発見詳細ページへ
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </Box>
   );
 }
