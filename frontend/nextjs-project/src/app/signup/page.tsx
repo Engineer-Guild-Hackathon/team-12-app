ccc48bedd6e8d7551fc026cb65087a23d95f9df4
Mayu Kosaka <ayumayou007@yahoo.ne.jp>
2025-09-10 22:27:20 +0900

    feat: 新規登録画面の実装

--- frontend/nextjs-project/src/app/signup/page.tsx
+++ frontend/nextjs-project/src/app/signup/page.tsx
@@ -1,5 +1,72 @@
-import { Box } from "@mui/material";
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { Box, Typography, TextField, Button } from "@mui/material";
+import { PiPlant } from "react-icons/pi";
 
 export default function Signup() {
-  return <Box>signup ページ</Box>;
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+
+  const handleSignUp = async () => {
+    if (username.trim() === "") {
+      alert("ユーザー名を入力してください");
+      return;
+    }
+
+    console.log("ユーザー登録中...");
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+    console.log("登録完了:", username);
+    router.push("/");
+  }
+
+
+  return (
+    <Box 
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        minHeight: "100vh",
+        paddingTop: "100px",
+        px: "10%",
+      }}>
+        <Box
+          sx={{ 
+            display: "flex",
+            alignItems: "center",
+            gap: "10px" }}
+        >
+          <PiPlant />
+          <Typography
+          >
+            新規登録
+          </Typography>
+        </Box>
+
+        <Box>
+          <Typography>
+            アカウント名
+          </Typography>
+          <TextField
+            variant="outlined"
+            placeholder="名前を入力してください"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+         />
+         <Typography>
+            ※全角8文字まで入力できます
+          </Typography>
+        </Box>
+        <Button
+          variant="contained"
+          onClick={handleSignUp}
+        >
+          <Typography>
+            登録
+          </Typography>
+        </Button>
+      </Box>
+  );
 }
