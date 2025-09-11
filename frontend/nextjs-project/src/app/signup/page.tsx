74a3b35a418c08a7c79a0d3d4996ea5a55d008fd
Mayu Kosaka <ayumayou007@yahoo.ne.jp>
2025-09-11 12:07:00 +0900

    feat: 新規登録画面の要素調整

--- frontend/nextjs-project/src/app/signup/page.tsx
+++ frontend/nextjs-project/src/app/signup/page.tsx
@@ -4,10 +4,13 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Box, Typography, TextField, Button } from "@mui/material";
 import { PiPlant } from "react-icons/pi";
+import { FaDiamond } from "react-icons/fa6";
+import { useTheme } from '@mui/material/styles';
 
 export default function Signup() {
   const router = useRouter();
   const [username, setUsername] = useState("");
+  const theme = useTheme();
 
   const handleSignUp = async () => {
     if (username.trim() === "") {
@@ -32,38 +35,118 @@ export default function Signup() {
         paddingTop: "100px",
         px: "10%",
       }}>
+
+        {/* ヘッダー部分 */}
         <Box
           sx={{ 
             display: "flex",
             alignItems: "center",
-            gap: "10px" }}
+            gap: "10px",
+            mb: "40px",
+            width: "100%",
+            justifyContent: "center",
+            pr: "24px",
+            pb: "10px",
+            borderBottom: "1px solid",
+            borderColor: "kinako.700",
+          }}
         >
-          <PiPlant />
+          <PiPlant color={theme.palette.yomogi[600]} size={24}/>
           <Typography
+            sx={{
+              fontSize: 24,
+              color: "kinako.800",
+            }}
           >
             新規登録
           </Typography>
         </Box>
 
-        <Box>
-          <Typography>
-            アカウント名
-          </Typography>
+        {/* ユーザー名入力部分 */}
+        <Box
+          sx={{
+            width: "100%",
+            alignItems: "center",
+            mb: "80px",
+        }} >
+          <Box
+            sx={{ 
+            display: "flex",
+            alignItems: "center",
+            gap: "6px",
+            mb: "12px",
+            }}>
+            <FaDiamond color={theme.palette.yomogi[600]} size={16}/>
+            <Typography >
+              アカウント名
+            </Typography>
+          </Box>
           <TextField
+            fullWidth
             variant="outlined"
             placeholder="名前を入力してください"
+           sx={{
+            // height: 64,
+            borderRadius: "8px",
+            backgroundColor: 'gray.100',
+            mb: "8px",
+            '& .MuiOutlinedInput-root': {
+              '&.Mui-focused': {
+                boxShadow: 'none', // フォーカス時のハイライトシャドウを消す
+              },
+              '& fieldset': {
+                border: 'none', // 通常時の枠線を消す
+              },
+              '&:hover fieldset': {
+                border: 'none', // ホバー時の枠線を消す
+              },
+              '&.Mui-focused fieldset': {
+                border: 'none', // フォーカス時の枠線を消す
+              },
+              '& .MuiOutlinedInput-input': {
+                height: '64px', // ① TextField全体の高さを指定
+                padding: '0 12px', // ② 上下のpaddingを0にして中央揃えを実現
+                boxSizing: 'border-box', // paddingを含めて高さを計算
+              },
+            },
+          }}
             value={username}
             onChange={(e) => setUsername(e.target.value)}
          />
-         <Typography>
+         <Typography
+            sx={{
+              fontSize: 14,
+              color: "kinako.900",
+            }}>
             ※全角8文字まで入力できます
           </Typography>
         </Box>
+
         <Button
           variant="contained"
           onClick={handleSignUp}
+          sx={{
+            width: "80%",
+            maxWidth: 340,
+            borderRadius: 200,
+            backgroundColor: "kinako.900",
+            color: "gray.100",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            lineHeight: 1.2,
+            py: 1.6,
+            gap: 1,
+            boxShadow: "none",
+            textTransform: "none",
+            "&:hover": {
+              transform: "translateY(-3px) scale(1.02)",
+              boxShadow: "none",
+            },
+          }}
         >
-          <Typography>
+          <Typography fontSize={20}>
             登録
           </Typography>
         </Button>
