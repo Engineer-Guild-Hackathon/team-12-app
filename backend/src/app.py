03d774f6ccc99584487973360ec646e360378368
Tomopu <m.poochian@gmail.com>
2025-09-12 21:08:25 +0900

    update: 全ての機能を統合

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from src.routes.image_route import image_bp
 from src.routes.img_analyze_route import img_analyze_bp
+from src.routes.post_route import post_bp
 
 app = Flask(__name__)
 
+app.register_blueprint(post_bp)
 app.register_blueprint(img_analyze_bp)
+app.register_blueprint(image_bp)
 
 
 @app.route("/")
