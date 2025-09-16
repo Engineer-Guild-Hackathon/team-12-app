f1637e8241b3e1890eebbcce4c07298e69a8efa0
CShoku <suzuki.chihiro.z5@elms.hokudai.ac.jp>
2025-09-16 19:27:53 +0900

    feat: filterからも呼び出せるように修正

--- frontend/nextjs-project/src/components/features/bottom-nav/RecordButton.tsx
+++ frontend/nextjs-project/src/components/features/bottom-nav/RecordButton.tsx
@@ -5,19 +5,13 @@ import { useDiscoveryCreationStore } from "@/stores/discoveryCreationStore";
 import { BottomNavigationAction, Box, Stack, Typography } from "@mui/material";
 import React from "react";
 import { PiCamera } from "react-icons/pi";
-import LoginGuideModal from "../auth/LoginGuideModal";
 
 export default function RecordButton() {
   const startCreation = useDiscoveryCreationStore(
     (state) => state.startCreation,
   );
-  const { user, isLoginGuideModalOpen, handleLoginGuideModal } = useAuthStore(
-    (state) => ({
-      user: state.user,
-      isLoginGuideModalOpen: state.isLoginGuideModalOpen,
-      handleLoginGuideModal: state.handleLoginGuideModal,
-    }),
-  );
+  const user = useAuthStore((s) => s.user);
+  const handleLoginGuideModal = useAuthStore((s) => s.handleLoginGuideModal);
   const handleStartCreation = () => {
     if (user !== null) {
       startCreation();
@@ -25,45 +19,39 @@ export default function RecordButton() {
       handleLoginGuideModal(true);
     }
   };
-  const closeModal = () => {
-    handleLoginGuideModal(false);
-  };
 
   return (
-    <>
-      <BottomNavigationAction
-        disableRipple
-        onClick={handleStartCreation}
-        icon={
-          <Box
+    <BottomNavigationAction
+      disableRipple
+      onClick={handleStartCreation}
+      icon={
+        <Box
+          sx={{
+            position: "absolute",
+            top: -28,
+            padding: "3px",
+            border: "2px solid",
+            borderColor: "yomogi.800",
+            borderRadius: "50%",
+            backgroundColor: "kinako.100",
+          }}
+        >
+          <Stack
+            alignItems="center"
+            justifyContent="center"
             sx={{
-              position: "absolute",
-              top: -28,
-              padding: "3px",
-              border: "2px solid",
-              borderColor: "yomogi.800",
+              backgroundColor: "yomogi.800",
               borderRadius: "50%",
-              backgroundColor: "kinako.100",
+              width: 84,
+              height: 84,
+              color: "gray.100",
             }}
           >
-            <Stack
-              alignItems="center"
-              justifyContent="center"
-              sx={{
-                backgroundColor: "yomogi.800",
-                borderRadius: "50%",
-                width: 84,
-                height: 84,
-                color: "gray.100",
-              }}
-            >
-              <PiCamera size={36} />
-              <Typography sx={{ fontSize: "0.875rem" }}>きろく</Typography>
-            </Stack>
-          </Box>
-        }
-      />
-      <LoginGuideModal open={isLoginGuideModalOpen} closeModal={closeModal} />
-    </>
+            <PiCamera size={36} />
+            <Typography sx={{ fontSize: "0.875rem" }}>きろく</Typography>
+          </Stack>
+        </Box>
+      }
+    />
   );
 }
