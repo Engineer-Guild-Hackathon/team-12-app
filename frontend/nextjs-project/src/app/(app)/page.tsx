e21ff378893a7343b1b946552a1f20d04b7139ad
Utaho Akashi <akashi.uta.1212@gmail.com>
2025-09-16 17:50:03 +0900

    fix: import文を修正

--- frontend/nextjs-project/src/app/(app)/page.tsx
+++ frontend/nextjs-project/src/app/(app)/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { Box, CircularProgress } from "@mui/material";
+import { Box } from "@mui/material";
 import HomeClient from "./client";
 
 // Suspenseのローディング中に表示するコンポーネント
