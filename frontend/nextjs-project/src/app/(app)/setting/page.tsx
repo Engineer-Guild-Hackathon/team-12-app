a61b8aa3d28c836a96c95af8e4270ef2ec0c0842
CShoku <suzuki.chihiro.z5@elms.hokudai.ac.jp>
2025-09-16 11:20:37 +0900

    fix: settingページに直接アクセスされたときの対策

--- frontend/nextjs-project/src/app/(app)/setting/page.tsx
+++ frontend/nextjs-project/src/app/(app)/setting/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import LoginGuideModal from "@/components/features/auth/LoginGuideModal";
 import SubmitButton from "@/components/ui/SubmitButton";
 import { useAuthStore } from "@/stores/authStore";
 import { Stack } from "@mui/material";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function SettingPage() {
   const { signOut, user } = useAuthStore();
@@ -14,12 +14,17 @@ export default function SettingPage() {
     alert("ログアウトしました");
     router.push("/");
   };
+
+  // 万が一URLに直接アクセスされたときの対策
+  useEffect(() => {
+    if (user === null) {
+      router.replace("/");
+    }
+  }, [user, router]);
+
   return (
-    <>
-      <Stack pt={2}>
-        <SubmitButton onClick={handleLogout}>ログアウト</SubmitButton>
-      </Stack>
-      <LoginGuideModal open={user === null} />
-    </>
+    <Stack pt={2}>
+      <SubmitButton onClick={handleLogout}>ログアウト</SubmitButton>
+    </Stack>
   );
 }
