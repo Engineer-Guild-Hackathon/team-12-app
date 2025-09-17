f8084b366f62dcfc46a9558bfd1d3a9e5350e443
Mayu Kosaka <ayumayou007@yahoo.ne.jp>
2025-09-17 22:57:23 +0900

    feat: ログイン済みの場合ログイン画面からリダイレクト

--- frontend/nextjs-project/src/app/login/page.tsx
+++ frontend/nextjs-project/src/app/login/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
 import LoginView from "@/components/features/auth/LoginView";
+import { useAuthStore } from "@/stores/authStore";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function LoginPage() {
+  // ログイン済みならマップにリダイレクト
+  const user = useAuthStore((state) => state.user);
+  const router = useRouter();
+  useEffect(() => {
+    if (user !== null) {
+      router.replace("/");
+    }
+  }, [user, router]);
+
   return <LoginView />;
 }
