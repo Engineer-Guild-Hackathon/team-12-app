5df6369d81aad3026b2a166f119c09f1ce1c3eac
CShoku <suzuki.chihiro.z5@elms.hokudai.ac.jp>
2025-09-17 18:55:13 +0900

    feat: モーダル実装した

--- frontend/nextjs-project/src/components/features/discovery-creation/DiscoveryDetailView.tsx
+++ frontend/nextjs-project/src/components/features/discovery-creation/DiscoveryDetailView.tsx
@@ -1,7 +1,15 @@
 "use client";
 
-import { Box, Button, Stack } from "@mui/material";
-import { IoLeaf, IoSearch } from "react-icons/io5";
+import {
+  Box,
+  Stack,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  Button,
+  Typography,
+} from "@mui/material";
+import { IoLeaf, IoSearch, IoClose } from "react-icons/io5";
 import QuestionBubble from "@/components/ui/QuestioinBubble";
 import Section from "@/components/ui/Section";
 import DiscoveryImage from "@/components/ui/DiscoveryImage";
@@ -30,7 +38,16 @@ export default function DiscoveryDetailView({
 }: DiscoveryDetailViewProps) {
   const user = useAuthStore((state) => state.user);
   const isPostOwner = user?.uid === post.user_id;
-  const { deleteDiscovery } = useDiscoveryDelete();
+  const {
+    deleteDiscovery,
+    isDeleteModalOpen,
+    openDeleteModal,
+    closeDeleteModal,
+  } = useDiscoveryDelete();
+  const handleDeleteDiscovery = () => {
+    deleteDiscovery(post.post_id);
+    closeDeleteModal();
+  };
   const isPWA = useIsPWA();
   const discoveryHeaderHeight = isPWA
     ? DISCOVERY_HEADER_HEIGHT
@@ -39,55 +56,129 @@ export default function DiscoveryDetailView({
   const { imageUrl, isLoading, isError } = useImage(post.img_id);
 
   return (
-    <Box sx={{ px: 3 }}>
-      <DiscoveryHeader iconName={iconName} formattedDate={formattedDate} />
-      <Stack
-        spacing={4}
-        sx={{
-          color: "kinako.900",
-          pt: `${discoveryHeaderHeight + 20}px`,
-          pb: "20px",
-        }}
-      >
-        {isLoading && <div>画像を読み込んでいます...</div>}
-        {isError && <div>画像の読み込みに失敗しました</div>}{" "}
-        {imageUrl && <DiscoveryImage src={imageUrl} alt={post.object_label} />}
-        {/* 2. 質問 */}
-        <QuestionBubble text={post.user_question} />
-        {/* 3. AIからの回答 (はっけん) */}
-        <Section icon={<IoLeaf size={32} />} title="はっけん">
-          {post.ai_answer}
-        </Section>
-        {/* 4. AIからの問い */}
-        <Section icon={<IoSearch size={32} />} title="問い">
-          {post.ai_question}
-        </Section>
-        {/* TODO: 仮で置いているので必ずfigmaを参照する */}
-        {/* 5. 投稿削除ボタン */}
-        {isPostOwner && (
-          <Button
-            variant="contained"
-            onClick={() => deleteDiscovery(post.post_id)}
-            autoFocus
-            sx={{
-              backgroundColor: "kinako.900",
-              color: "white",
-              "&:hover": {
-                backgroundColor: "kinako.700",
-              },
-              padding: "8px 16px",
-              height: "56px",
-              width: "80%",
-              borderRadius: "200px",
-              textTransform: "none",
-              fontSize: "20px",
-              alignSelf: "center",
-            }}
-          >
-            この投稿を削除する
-          </Button>
-        )}
-      </Stack>
-    </Box>
+    <>
+      <Box sx={{ px: 3 }}>
+        <DiscoveryHeader
+          iconName={iconName}
+          formattedDate={formattedDate}
+          isShownDeleteButton={isPostOwner}
+          onDeleteClick={openDeleteModal}
+        />
+        <Stack
+          spacing={4}
+          sx={{
+            color: "kinako.900",
+            pt: `${discoveryHeaderHeight + 20}px`,
+            pb: "20px",
+          }}
+        >
+          {isLoading && <div>画像を読み込んでいます...</div>}
+          {isError && <div>画像の読み込みに失敗しました</div>}{" "}
+          {imageUrl && (
+            <DiscoveryImage src={imageUrl} alt={post.object_label} />
+          )}
+          {/* 2. 質問 */}
+          <QuestionBubble text={post.user_question} />
+          {/* 3. AIからの回答 (はっけん) */}
+          <Section icon={<IoLeaf size={32} />} title="はっけん">
+            {post.ai_answer}
+          </Section>
+          {/* 4. AIからの問い */}
+          <Section icon={<IoSearch size={32} />} title="問い">
+            {post.ai_question}
+          </Section>
+        </Stack>
+      </Box>
+      <DeleteConfirmModal
+        open={isDeleteModalOpen}
+        closeModal={closeDeleteModal}
+        deleteDiscovery={handleDeleteDiscovery}
+      />
+    </>
   );
 }
+
+const DeleteConfirmModal = ({
+  open,
+  closeModal,
+  deleteDiscovery,
+}: {
+  open: boolean;
+  closeModal: () => void;
+  deleteDiscovery: () => void;
+}) => {
+  return (
+    <Dialog
+      open={open}
+      slotProps={{
+        paper: {
+          sx: {
+            width: "400px",
+            p: "40px 32px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            gap: "20px",
+            borderRadius: "16px",
+          },
+        },
+      }}
+      keepMounted
+    >
+      <Box
+        sx={{ display: "flex", flexDirection: "row-reverse", height: "12px" }}
+      >
+        <IoClose size={20} onClick={closeModal} cursor="pointer" />
+      </Box>
+      <DialogContent
+        sx={{
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "column",
+          gap: "12px",
+          padding: "0",
+          overflow: "visible",
+          mb: "20px",
+        }}
+      >
+        <Typography
+          variant="h2"
+          component="h2"
+          sx={{ textAlign: "center", fontSize: 24, color: "kinako.900" }}
+        >
+          はっけんを削除しますか？
+        </Typography>
+        <Typography
+          variant="h4"
+          component="h4"
+          sx={{ fontSize: 14, color: "kinako.800" }}
+        >
+          一度削除すると、元に戻すことはできません
+        </Typography>
+      </DialogContent>
+      <DialogActions sx={{ justifyContent: "center", padding: "0" }}>
+        <Button
+          variant="contained"
+          onClick={deleteDiscovery}
+          autoFocus
+          sx={{
+            backgroundColor: "kinako.900",
+            color: "white",
+            "&:hover": {
+              backgroundColor: "kinako.700",
+            },
+            padding: "8px 16px",
+            height: "56px",
+            width: "100%",
+            borderRadius: "200px",
+            textTransform: "none",
+            fontSize: "20px",
+            alignSelf: "center",
+          }}
+        >
+          削除する
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
--- frontend/nextjs-project/src/hooks/useDiscoveryDelete.ts
+++ frontend/nextjs-project/src/hooks/useDiscoveryDelete.ts
@@ -1,32 +1,38 @@
 import { deletePostAction } from "@/app/actions/deleteActions";
 import { useRouter } from "next/navigation";
-import { startTransition } from "react";
+import { useState } from "react";
 
 export const useDiscoveryDelete = () => {
   const router = useRouter();
-  const deleteDiscovery = (post_id: string) => {
-    const confirm = window.confirm(
-      "本当にこの投稿を削除しますか？この操作は元に戻せません。",
-    );
-    if (!confirm) return;
-
-    startTransition(async () => {
-      // TODO: ローディングマークつける
-      const result = await deletePostAction(post_id);
-      if (!result.error && !result.data) {
-        alert("不明なエラーが発生しました。もう一度お試しください。");
-        return;
-      }
-      if (result.data) {
-        alert("投稿が削除されました");
-        router.back();
-        return;
-      }
-      if (result.error) {
-        alert(`投稿の削除に失敗しました ${result.error}`);
-      }
-    });
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false);
+  const openDeleteModal = () => {
+    setIsDeleteModalOpen(true);
+  };
+  const closeDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
+  const deleteDiscovery = async (post_id: string) => {
+    // TODO: ローディングマークつける
+    // server actionsを直接呼び出す
+    const result = await deletePostAction(post_id);
+    if (!result.error && !result.data) {
+      alert("不明なエラーが発生しました。もう一度お試しください。");
+      return;
+    }
+    if (result.data) {
+      alert("投稿が削除されました");
+      router.back();
+      return;
+    }
+    if (result.error) {
+      alert(`投稿の削除に失敗しました ${result.error}`);
+    }
   };
 
-  return { deleteDiscovery };
+  return {
+    deleteDiscovery,
+    isDeleteModalOpen,
+    openDeleteModal,
+    closeDeleteModal,
+  };
 };
